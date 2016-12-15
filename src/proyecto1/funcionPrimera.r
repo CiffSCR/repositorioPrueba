c984d83375395b50e493a00b8fe3199d9bd9fe27
Sergio Castro <sergiocastro@campusciff.net>
2016-12-15 14:37:03 +0100

    20161215 14:33| V 0.0.0 commit inicial

---
+++ src/proyecto1/funcionPrimera.r
@@ -0,0 +1,7 @@
+# V 1.0.0 version inicial para pruebas de repositorio
+# proyecto 1
+library(lpsolve)
+prueba1<-function(paramA, paramB){
+  resultado<-paramA+paramB
+  return (resultado)
+}
---
+++ src/proyecto2/funcionSegunda.r
@@ -0,0 +1,7 @@
+# V 1.0.0 version inicial para pruebas de repositorio
+# proyecto 2
+library(lpsolve)
+prueba2<-function(paramA, paramB){
+  resultado<-paramA*paramB
+  return (resultado)
+}
